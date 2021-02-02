6f9565bd1c38ac69024a2699209f1ad43ad2b568
Jimmy Juarez <jimmy@MacBook-Pro.local>
2021-02-02 01:39:49 -0800

    Add welcome menu

--- index.js
+++ index.js
@@ -16,8 +16,30 @@ client.on('message', message =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     if(command === "welcome"){
-        let welcome = "Welome to the **SHPE Tech Comittee** Server";
-        message.channel.send(welcome,{files:["./shpelogo.jpg"]});
+        let welcome = "Welcome to the **SHPE Tech Comittee** Server";
+        message.channel.send(welcome,{files:["./2021Tech.png"]});
+    }
+
+    const rolesDescrpt = `Choose your roles in the <#806079231645057034> text-channel.`;
+    const announceDescrpt = `Keep up with <#762902109451583488> for important updates about upcoming events, workshops, and application deadlines! We will seldom use the @everyone ping, so please do not mute us But the @everyone ping will be used right before an event starts or when a deadline is approaching.`;
+    const recapDescrpt = `Missed a workshop or an event? We'll have the resources for you posted for you to look through on your own time. Recordings may be released a bit later, but look in that text-channel for updates as to when we post it :smile:`
+    if(command === "a"){
+        let rolesEmbed = new Discord.MessageEmbed()
+            .setColor('#f0633d')
+            .setTitle('Roles')
+            .setDescription(rolesDescrpt);
+        
+        let anouncementsEmbed = new Discord.MessageEmbed()
+            .setColor('#74aabe')
+            .setTitle('Announcements')
+            .setDescription(announceDescrpt);
+
+        let eventsEmbed = new Discord.MessageEmbed()
+            .setColor('#f3f3f3')
+            .setTitle('Recaps')
+            .setDescription(announceDescrpt);
+
+        message.channel.send(rolesEmbed).then(msg => {message.channel.send(anouncementsEmbed)}).then(msg => {message.channel.send(eventsEmbed)});
     }
     
 });
