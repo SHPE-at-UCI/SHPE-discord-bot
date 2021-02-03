4833a2642a32afc95035ee7cbc3410541aa642eb
SarahCatania <sarahcatania9@gmail.com>
2021-02-03 00:12:10 -0800

    Added Reaction Roles

---
+++ commands/ReactionRole.js
@@ -0,0 +1,98 @@
+module.exports = {
+    name: 'reactionrole',
+    description: "Sets up roles and committees",
+
+    async execute(message, args, Discord, client) {
+        const channel = '762902109451583488';
+        const shpetinas = message.guild.roles.cache.find(role => role.name === "shpetinas");
+        const professional = message.guild.roles.cache.find(role => role.name === "professional");
+        const shpejr = message.guild.roles.cache.find(role => role.name === "shpe jr. mentor");
+        const techy = message.guild.roles.cache.find(role => role.name === "techy");
+        const eventsPlanning = message.guild.roles.cache.find(role => role.name === "events planning committee");
+
+        const shpetinasEmoji = '<:shpetinas:806086863244623883>';
+        const professionalEmoji = '<:professional:806086863013412955>';
+        const shpejrEmoji = '<:shpejr:806086863042641920>';
+        const techEmoji = '<:tech:806086863025995778>';
+        const eventsEmoji = '<:EPC:806300282492289095>';
+
+        function emoji(id)
+        {
+            return client.emojis.get(id).toString();
+        }
+
+        let embed = new Discord.MessageEmbed()
+            .setColor('#f0633d')
+            .setTitle('Choose your committee!')
+            .setDescription('Choosing your committee will alow you to interact with your fellow committee members! \n\n'
+                + `${shpetinasEmoji} for Shpetinas Committee\n`
+                + `${professionalEmoji} for Professional Committee\n`
+                + `${shpejrEmoji} for Shpe Jr. Committee\n`
+                + `${techEmoji} for Tech Committee\n`
+                + `${eventsEmoji} for Events Planning Committee`);
+
+        let messageEmbed = await message.channel.send(embed);
+        messageEmbed.react(shpetinasEmoji);
+        messageEmbed.react(professionalEmoji);
+        messageEmbed.react(shpejrEmoji);
+        messageEmbed.react(techEmoji);
+        messageEmbed.react(eventsEmoji);
+
+        client.on('messageReactionAdd', async (reaction, user) => {
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.id === shpetinasEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(shpetinas);
+                }
+                if (reaction.emoji.id === professionalEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(professional);
+                }
+                if (reaction.emoji.id === shpejrEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(shpejr);
+                }
+                if (reaction.emoji.id === techEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(techy);
+                }
+                if (reaction.emoji.id === eventsEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(eventsPlanning);
+                }
+            } else {
+                return;
+            }
+
+        });
+
+        client.on('messageReactionRemove', async (reaction, user) => {
+
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+
+
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.id === shpetinasEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(shpetinas);
+                }
+                if (reaction.emoji.id === professionalEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(professional);
+                }
+                if (reaction.emoji.id === shpejrEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(shpejr);
+                }
+                if (reaction.emoji.id === techEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(techy);
+                }
+                if (reaction.emoji.id === eventsEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(eventsPlanning);
+                }
+            } else {
+                return;
+            }
+        });
+    }
+}
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 dotenv.config();
 
 const prefix = '!';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log('SHPE bot is online!')
@@ -20,15 +31,15 @@ client.on('message', message =>{
         message.channel.send(welcome,{files:["./2021Tech.png"]});
     }
 
-    const rolesDescrpt = `Choose your roles in the <#806079231645057034> text-channel.`;
+    const rolesDescrpt = `Choose your roles & committees in the <#806079231645057034> text-channel!`;
     const announceDescrpt = `Keep up with <#762902109451583488> for important updates about upcoming events, workshops, and application deadlines! We will seldom use the @everyone ping, so please do not mute us But the @everyone ping will be used right before an event starts or when a deadline is approaching.`;
     const recapDescrpt = `Missed a workshop or an event? We'll have the resources for you posted for you to look through on your own time. Recordings may be released a bit later, but look in that text-channel for updates as to when we post it :smile:`
-    if(command === "a"){
+    if (command === "s") {
         let rolesEmbed = new Discord.MessageEmbed()
             .setColor('#f0633d')
             .setTitle('Roles')
             .setDescription(rolesDescrpt);
-        
+
         let anouncementsEmbed = new Discord.MessageEmbed()
             .setColor('#74aabe')
             .setTitle('Announcements')
@@ -39,9 +50,15 @@ client.on('message', message =>{
             .setTitle('Recaps')
             .setDescription(announceDescrpt);
 
-        message.channel.send(rolesEmbed).then(msg => {message.channel.send(anouncementsEmbed)}).then(msg => {message.channel.send(eventsEmbed)});
+        message.channel.send(rolesEmbed).then(msg => { message.channel.send(anouncementsEmbed) }).then(msg => { message.channel.send(eventsEmbed) });
     }
+    if (command === 'reactionrole') {
+        client.commands.get('reactionrole').execute(message, args, Discord, client);
+    }
+
+
     
 });
+
 
 client.login(process.env.DISCORD_KEY);
